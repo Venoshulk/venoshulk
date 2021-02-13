3cc1862d085d65192d10b13220ed052c2fd58d84
Venoshulk <arthurmousatov@gmail.com>
2021-02-13 18:39:02 -0500

    New Web Design

---
+++ scripts/lazyLoad.js
@@ -0,0 +1,23 @@
+let obsOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.1
+}
+
+function lazyCall(entries, obs){
+    entries.forEach(element => {
+        if(element.isIntersecting){
+            element.target.src = element.target.dataset.src;
+            obs.unobserve(element.target);
+        }
+    });
+}
+
+function main(){
+    let observer = new IntersectionObserver(lazyCall, obsOptions);
+    console.log(observer);
+
+    document.querySelectorAll('img').forEach(img => {observer.observe(img)});
+}
+
+window.onload = main;
