558b0b79ba118ca2d2f2816c6553068e7deff331
ArthurMousatov <57199800+ArthurMousatov@users.noreply.github.com>
2023-07-10 23:48:34 -0400

    Modified card/hero-intro css

--- angular-github-page/src/app/main-page/main-page.component.ts
+++ angular-github-page/src/app/main-page/main-page.component.ts
@@ -11,7 +11,7 @@ export class MainPageComponent {
     new Card('Coveo Blitz 2021', 'Team Hackathon', '/assets/imgs/blitz.png'),
     new Card('Coveo Blitz 2022', 'Team Hackathon', '/assets/imgs/blitz2022.jpg'),
     new Card('Mogile Technologies', 'Full-stack internship using MEVN (Mongo, Express, Vue, Node)', '/assets/imgs/mogile.jpg'),
-    new Card('Morgan Stanley', 'Python Development Focusing on SRE tooling', '/assets/imgs/ms.jpg', null, true)
+    new Card('Morgan Stanley', 'Python Development Focusing on SRE tooling', '/assets/imgs/ms.jpg')
   ]
   public projects = [
     new Card('Discord Notes', 'Multi-user drawing application using Socket.IO, NodeJS, Bootstrap, Heroku & Discord.py', '/assets/imgs/draw.png', 'https://github.com/ArthurMousatov/DiscordNotes'),
